1690536e9b03fd17d30a565486365a0cf9db47ff
WeiXiang123 <jerry@tootootekiMacBook-Air.local>
2015-02-02 15:37:29 +0800

    use dispatch
    download weather and show info

---
+++ Whats The Weather/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  Whats The Weather
+//
+//  Created by WeiXiang on 15/2/2.
+//  Copyright (c) 2015年 WeiXiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userCity: UITextField!
+    @IBOutlet weak var resultLable: UILabel!
+    
+    @IBAction func findWeather(sender: UIButton) {
+
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/" + userCity.text.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+
+        if url != nil {
+
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+
+                var urlError = false
+
+                var weather = ""
+
+                if error == nil {
+
+                    var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding) as NSString!
+
+                    var urlContentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+
+                    if urlContentArray.count > 1 {
+
+                        var weatherArray = urlContentArray[1].componentsSeparatedByString("</span>")
+
+                        weather = weatherArray[0] as String
+
+                        weather = weather.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+
+                    } else {
+
+                        urlError = true
+
+                    }
+
+
+
+                } else {
+
+                    urlError = true
+
+                }
+//ui must in main thread
+                dispatch_async(dispatch_get_main_queue()) {
+
+                    if urlError == true {
+
+                        self.showError()
+
+                    } else {
+
+                        self.resultLable.text = weather
+
+                    }
+                }
+
+            })
+
+            task.resume()
+
+
+        } else {
+
+            showError()
+
+        }
+
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func showError() {
+        resultLable.text = "was not find the weather for " + userCity.text + ", please try it again"
+
+    }
+
+}
+
